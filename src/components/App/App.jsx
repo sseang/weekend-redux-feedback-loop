0a6b4a76b4e18d6ee0133b522ff0ef801fc6f709
sseang <142060734+sseang@users.noreply.github.com>
2024-02-05 19:50:18 -0600

    created feeling form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
+
 import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [inputFeeling, setInputFeeling] = useState('');
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <form>
+        <label>Feeling? </label>
+        <input
+          data-testid="input"
+          onChange={(event) => setInputFeeling(event.target.value)}
+          value={inputFeeling}
+        />
+        <button data-testid="next">NEXT</button>
+      </form>
     </div>
   );
 }
