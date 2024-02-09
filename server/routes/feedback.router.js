f8ac128a70648a6ed2251b8dc62ece832706351a
sseang <142060734+sseang@users.noreply.github.com>
2024-02-08 19:03:37 -0600

    added TODO notes

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -1,25 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../modules/pool')
-
+const pool = require('../modules/pool');
 
 // TODO: This route adds a new feedback entry
-router.post('/', (req, res) => {
+router.post('/', (req, res) => {});
 
-})
-
+// TODO: This route RESETS for new feedback. See TODO for "TRUNCATE"
+router.delete('/', (req, res) => {});
 
 // DO NOT EDIT THIS ROUTE
 // This route must return all feedback.
 router.get('/', (req, res) => {
-    console.log('testing')
-    const sqlText = `SELECT * FROM "feedback" ORDER BY "id"`;
-    pool.query(sqlText).then(result => {
-        res.send(result.rows)
-    }).catch(err => {
-        console.log(err);
-        res.sendStatus(500);
+  console.log('testing');
+  const sqlText = `SELECT * FROM "feedback" ORDER BY "id"`;
+  pool
+    .query(sqlText)
+    .then((result) => {
+      res.send(result.rows);
     })
-})
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = router;
---
+++ src/components/Review/Review.jsx
@@ -0,0 +1,1 @@
+// TODO: add submit and axios POST
