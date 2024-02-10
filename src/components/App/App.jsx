ee3caeb7b415fa71158ffdd92586dfee4b004166
sseang <142060734+sseang@users.noreply.github.com>
2024-02-09 18:25:40 -0600

    removed feeling function to Feeling component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,51 +7,24 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 //import components
 import Header from '../Header/Header';
-//import Understanding from '../Understanding/Understanding';
+import Feeling from '../Feeling/Feeling';
+import Understanding from '../Understanding/Understanding';
 
 //send axios post instead?
 import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [inputFeeling, setInputFeeling] = useState('');
-
-  const dispatch = useDispatch();
-
-  const handleSubmitFeeling = (event) => {
-    console.log('In my FEELINGS');
-    //prevent refresh
-    event.preventDefault();
-    //action to storeInstance
-    dispatch({
-      //payload to store: REMEMBER to create reducer in store.js and add wrap PROVIDER!!
-      type: 'FEELING_COLUMN_ADD',
-      payload: {
-        feeling: inputFeeling,
-      },
-    });
-    setInputFeeling('');
-  };
-
-  //onchange event
-  const handleAddFeeling = (event) => {
-    setInputFeeling(event.target.value);
-  };
-
   return (
     <div className="App">
-      <Header />
-      <form>
-        <label>Feeling? </label>
-        <input
-          data-testid="input"
-          onChange={handleAddFeeling}
-          value={inputFeeling}
-        />
-        <button data-testid="next" type="button" onClick={handleSubmitFeeling}>
-          NEXT
-        </button>
-      </form>
+      <Router>
+        <Header />
+        <Feeling />
+
+        {/* <Route path="/understand" exact>
+          <Understanding />
+        </Route> */}
+      </Router>
     </div>
   );
 }
---
+++ src/components/Feeling/Feeling.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useState } from 'react';
+//failing when calling dispatch
+//add use dispatch
+import { useDispatch } from 'react-redux';
+
+function Feeling() {
+  const [inputFeeling, setInputFeeling] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleSubmitFeeling = (event) => {
+    console.log('In my FEELINGS');
+    //prevent refresh
+    event.preventDefault();
+    //action to storeInstance
+    dispatch({
+      //payload to store: REMEMBER to create reducer in store.js and add wrap PROVIDER!!
+      type: 'FEELING_COLUMN_ADD',
+      payload: {
+        feeling: inputFeeling,
+      },
+    });
+    setInputFeeling('');
+  };
+
+  //onchange event
+  const handleAddFeeling = (event) => {
+    setInputFeeling(event.target.value);
+  };
+
+  return (
+    <div>
+      <form>
+        <label>Feeling? </label>
+        <input
+          data-testid="input"
+          onChange={handleAddFeeling}
+          value={inputFeeling}
+        />
+        <button data-testid="next" type="button" onClick={handleSubmitFeeling}>
+          NEXT
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default Feeling;
